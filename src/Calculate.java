c98157b4f11f2a41cca4c9ebc7d68c6031ae2828
Christa Stephens <ChristaStephens@outlook.com>
2019-03-04 15:22:37 -0500

    creating variables

--- src/Calculate.java
+++ src/Calculate.java
@@ -2,7 +2,13 @@
 public class Calculate {
 
 	public static void main(String[] args) {
-		// Goal 
+		//Goal: have user enter values of length & width
+		//Display the perimeter of the perimeter
+		//Ask if the user wants to continue
+		
+		double length;
+		double width; 
+		double perimeter;
 
 	}
 
